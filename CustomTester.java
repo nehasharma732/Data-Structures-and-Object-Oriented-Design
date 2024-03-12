724ee187a4b19d9f4a305fbe70ca5930448ff978
Neha Sharma <103093902+nehasharma732@users.noreply.github.com>
2024-03-11 17:39:41 -0700

    Stack & queue implementation

--- CustomTester.java
+++ CustomTester.java
@@ -1,173 +1,257 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
-
 import static org.junit.Assert.*;
-
 import org.junit.*;
 
 public class CustomTester {
 
-    Student studentTester1, studentTester2;
-    Course courseTester1;
-    Sanctuary sanctuaryTester1, sanctuaryTester2;
-
-    @Before
-    public void setup() throws Exception {
-
-        studentTester1 = new Student("Neha", "Sharma", "A16632613");
-        studentTester2 = new Student("Neha", "Sharma", "A11111111");
-
-        courseTester1 = new Course("CSE", "CSE 12", "data structures", 1);
-
-        sanctuaryTester1 = new Sanctuary(4, 2);
-        sanctuaryTester2 = new Sanctuary(4, 2);
-
+    static void initDeque(MyDeque<Integer> deque, Object[] data, int size,
+            int front, int rear) {
+        deque.data = data;
+        deque.size = size;
+        deque.front = front;
+        deque.rear = rear;
     }
 
     @Test
-    public void testStudentEquals() {
+    public void testDequeConstructor() {
 
-        assertThrows(IllegalArgumentException.class, () -> new Student(null, null, null));
-
-        assertSame(false, studentTester1.equals("hello"));
+        // check if it throws exception
+        assertThrows(IllegalArgumentException.class, () -> new MyDeque<>(-1));
     }
 
     @Test
-    public void testStudentCompareTo() {
+    public void testDequeExpandCapacity() {
 
-        assertTrue((studentTester2.compareTo(studentTester1) < 0));
+        MyDeque<Integer> deque = new MyDeque<>(5);
 
-    }
+        Integer[] orig = { 3, 4, 5, 1, 2 };
+        initDeque(deque, orig, 5, 3, 2);
 
-    @Test
-    public void testCourseDrop() {
+        Integer[] expanded = { 1, 2, 3, 4, 5, null, null, null, null, null };
 
-        courseTester1.enroll(studentTester1); // add one student
+        deque.expandCapacity();
 
-        assertTrue((courseTester1.drop(studentTester2) == false)); // drop nonexistent student
-
-        assertThrows(IllegalArgumentException.class, () -> courseTester1.drop(null));
-
-        assertSame(1, courseTester1.getEnrolledCount());
-
-        assertSame(true, courseTester1.drop(studentTester1));
-
-        assertSame(0, courseTester1.getEnrolledCount());
-    }
-
-    @Test
-    public void testCourseEnroll() {
-
-        courseTester1.enroll(studentTester1); // add one student
-
-        assertTrue((courseTester1.enroll(studentTester2) == false));
-
-        assertThrows(IllegalArgumentException.class, () -> courseTester1.enroll(null));
-
-        assertSame(1, courseTester1.getEnrolledCount());
-
-        courseTester1.drop(studentTester1);
-
-        assertSame(0, courseTester1.getEnrolledCount());
-    }
-
-    @Test
-    public void testCourseGetRoster() {
-
-        ArrayList<Student> expectedRoster = new ArrayList<>();
-
-        // Generate and enroll 100 students
-        for (int i = 1; i <= 100; i++) {
-
-            Student student = new Student("FirstName" + i, "LastName" + i, "A" + String.format("%08d", i));
-            courseTester1.enroll(student);
-            expectedRoster.add(student);
+        for (int i = 0; i < 10; i++) {
+            assertEquals("Deque structure should be maintained",
+                    expanded[i], deque.data[i]);
         }
-
-        Collections.sort(expectedRoster);
-
-        // ArrayList<Student> actualRoster = courseTester1.getRoster();
-
-        // assertEquals("The expected roster should match the actual roster",
-        // expectedRoster, actualRoster);
-    }
-
-    @Test
-    public void testSanctConstructor() {
-
-        assertThrows(IllegalArgumentException.class, () -> new Sanctuary(-20, 10));
+        assertSame(deque.size, 5);
+        assertSame(deque.front, 0);
+        assertSame(deque.rear, 4);
 
     }
 
     @Test
-    public void testSanctRescuePartial() {
+    public void testDequeAddFirst() {
 
-        sanctuaryTester1.rescue("moose", 3);
+        // Case 1
+        MyDeque<Integer> deque = new MyDeque<>(5);
 
-        assertSame(4, sanctuaryTester1.rescue("moose", 5)); // returns num rejected
+        Integer[] orig = { 1, 2, 3, 4, 5 };
+        initDeque(deque, orig, 5, 0, 4);
+
+        Integer[] finalOrdering = { 1, 2, 3, 4, 5, null, null, null, null, 0 };
+
+        deque.addFirst(0);
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals("Deque structure should be maintained",
+                    finalOrdering[i], deque.data[i]);
+        }
+        assertSame(deque.size, 6);
+        assertSame(deque.front, 9);
+        assertSame(deque.rear, 4);
+
+        // Case 2
+        MyDeque<Integer> deque2 = new MyDeque<>(5);
+
+        Integer[] orig2 = { 0, null, null, null, null };
+        initDeque(deque2, orig2, 1, 0, 0);
+
+        Integer[] finalOrdering2 = { 0, null, null, null, 10 };
+
+        deque2.addFirst(10);
+
+        for (int i = 0; i < 5; i++) {
+            assertEquals("Deque structure should be maintained",
+                    finalOrdering2[i], deque2.data[i]);
+        }
+        assertSame(deque2.size, 2);
+        assertSame(deque2.front, 4);
+        assertSame(deque2.rear, 0);
+
+        // Case 3
+        MyDeque<Integer> deque3 = new MyDeque<>(5);
+
+        Integer[] orig3 = { null, null, null, null, null };
+        initDeque(deque3, orig3, 0, 4, 4);
+
+        deque3.addFirst(5);
+
+        Integer[] finalOrdering3 = { null, null, null, null, 5 };
+
+        for (int i = 0; i < 5; i++) {
+            assertEquals("Deque structure should be maintained",
+                    finalOrdering3[i], deque3.data[i]);
+        }
+        assertSame(deque3.size, 1);
+        assertSame(deque3.front, 4);
+        assertSame(deque3.rear, 4);
 
     }
 
     @Test
-    public void testSanctRescueMaxSpecies() {
+    public void testDequeAddLast() {
 
-        sanctuaryTester1.rescue("moose", 1);
-        sanctuaryTester1.rescue("tiger", 2);
+        // Case 1
+        MyDeque<Integer> deque = new MyDeque<>(10);
 
-        int rejected = sanctuaryTester1.rescue("monkey", 1);
+        Integer[] orig = { null, 2, 3, 4, 5, null, null, null, null, null };
+        initDeque(deque, orig, 4, 1, 4);
 
-        assertSame(1, rejected);
+        Integer[] finalOrdering = { null, 2, 3, 4, 5, 6, null, null, null, null };
 
-        int mooseCount = sanctuaryTester1.countForSpecies("moose");
-        int tigerCount = sanctuaryTester1.countForSpecies("tiger");
-        int totalAnimals = sanctuaryTester1.getTotalAnimals();
-        int totalSpecies = sanctuaryTester1.getTotalSpecies();
+        deque.addLast(6);
 
-        assertSame(1, mooseCount);
-        assertSame(2, tigerCount);
-        assertSame(3, totalAnimals);
-        assertSame(2, totalSpecies);
+        for (int i = 0; i < 10; i++) {
+            assertEquals("Deque structure should be maintained",
+                    finalOrdering[i], deque.data[i]);
+        }
+        assertSame(deque.size, 5);
+        assertSame(deque.front, 1);
+        assertSame(deque.rear, 5);
 
-        // Verify no entry for "monkey" since its rescue was rejected
-        int monkeyCount = sanctuaryTester1.countForSpecies("monkey");
-        Assert.assertEquals("Monkey should not have been added", 0, monkeyCount);
+        // Case 2
+        MyDeque<Integer> deque2 = new MyDeque<>(5);
 
-    }
+        Integer[] orig2 = { null, null, null, null, null };
+        initDeque(deque2, orig2, 0, 0, 0);
 
-    @Test
-    public void testSanctReleasePartial() {
+        Integer[] finalOrdering2 = { 0, null, null, null, null };
 
-        sanctuaryTester1.rescue("moose", 1);
-        sanctuaryTester1.rescue("tiger", 3);
-        sanctuaryTester1.release("tiger", 2);
+        deque2.addLast(0);
 
-        assertSame(1, sanctuaryTester1.countForSpecies("tiger"));
-
-    }
-
-    @Test
-    public void testSanctReleaseTooMany() {
-
-        sanctuaryTester1.rescue("moose", 1);
-        sanctuaryTester1.rescue("tiger", 3);
-
-        assertThrows(IllegalArgumentException.class,
-                () -> sanctuaryTester1.release("tiger", 5));
+        for (int i = 0; i < 5; i++) {
+            assertEquals(finalOrdering2[i], deque2.data[i]);
+        }
+        assertSame(deque2.size, 1);
+        assertSame(deque2.front, 0);
+        assertSame(deque2.rear, 0);
 
     }
 
     @Test
-    public void testSanctHelpClosingSanctuaryPartial() {
+    public void testDequeRemoveFirst() {
 
-        sanctuaryTester1.rescue("moose", 2);
-        sanctuaryTester1.rescue("tiger", 1);
+        // Case 1
+        MyDeque<Integer> deque = new MyDeque<>(10);
 
-        sanctuaryTester2.rescue("moose", 2);
-        sanctuaryTester2.rescue("tiger", 1);
+        Integer[] orig = { 1, 2, 3, 4, 5, null, null, null, null, 0 };
+        initDeque(deque, orig, 6, 9, 4);
 
-        assertSame(1, sanctuaryTester1.helpClosingSanctuary(sanctuaryTester2));
+        Integer[] finalOrdering = { 1, 2, 3, 4, 5, null, null, null, null, null };
+
+        deque.removeFirst();
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals("Deque structure should be maintained",
+                    finalOrdering[i], deque.data[i]);
+        }
+        assertSame(deque.size, 5);
+        assertSame(deque.front, 0);
+        assertSame(deque.rear, 4);
+
+        deque.removeFirst(); // call again
+
+        Integer[] finalOrdering2 = { null, 2, 3, 4, 5, null, null, null, null, null };
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals(finalOrdering2[i], deque.data[i]);
+        }
+        assertSame(deque.size, 4);
+        assertSame(deque.front, 1);
+        assertSame(deque.rear, 4);
+    }
+
+    @Test
+    public void testDequeRemoveLast() {
+
+        // Case 1
+        MyDeque<Integer> deque = new MyDeque<>(5);
+
+        Integer[] orig = { 0, null, null, null, 10 };
+        initDeque(deque, orig, 2, 4, 0);
+
+        deque.removeLast();
+
+        Integer[] finalOrdering = { null, null, null, null, 10 };
+
+        for (int i = 0; i < 5; i++) {
+            assertEquals("Deque structure should be maintained",
+                    finalOrdering[i], deque.data[i]);
+        }
+        assertSame(deque.size, 1);
+        assertSame(deque.front, 4);
+        assertSame(deque.rear, 4);
+
+        deque.removeLast();
+
+        Integer[] finalOrdering2 = { null, null, null, null, null };
+
+        for (int i = 0; i < 5; i++) {
+            assertEquals(finalOrdering2[i], deque.data[i]);
+        }
+        assertSame(deque.size, 0);
+        assertSame(deque.front, 4);
+        assertSame(deque.rear, 4);
+    }
+
+    @Test
+    public void testStack() {
+
+        MyStack<String> stack = new MyStack<>(15);
+
+        stack.push("First");
+        stack.push("Second");
+        stack.push("Third");
+
+        assertSame("Third", stack.pop());
+        assertSame("Second", stack.pop());
+        assertSame("First", stack.pop());
+
+        assertTrue(stack.empty());
 
     }
+
+    @Test
+    public void testQueue() {
+
+        MyQueue<Integer> queue = new MyQueue<>(10);
+
+        queue.enqueue(5);
+        queue.enqueue(7);
+        queue.enqueue(4);
+
+        assertSame(5, queue.dequeue());
+        assertSame(7, queue.dequeue());
+        assertSame(4, queue.dequeue());
+    }
+
+    @Test
+    public void testavgDiffMonotonicIncreasing() {
+
+        MyAlgorithm algorithmInstance = new MyAlgorithm();
+
+        int[] intArrShort = { 4, 1, 3, 5, 7 };
+        int[] intArrLarge = { 1, 2, 3, 3, 4, 5, 4, 5 };
+        int[] intArrLarge2 = { 4, 3, 2, 1, 4, 3, 3, 3 };
+        int[] intArr1Arg = { 1 };
+        int[] intArrNoArg = {};
+
+        assertEquals(1.5, algorithmInstance.avgDiffMonotonicIncreasing(intArrShort), 0.0001);
+        assertEquals(1.0, algorithmInstance.avgDiffMonotonicIncreasing(intArrLarge), 0.0001);
+        assertEquals(0.0, algorithmInstance.avgDiffMonotonicIncreasing(intArrLarge2), 0.0001);
+        assertEquals(0.0, algorithmInstance.avgDiffMonotonicIncreasing(intArr1Arg), 0.0001);
+        assertEquals(0.0, algorithmInstance.avgDiffMonotonicIncreasing(intArrNoArg), 0.0001);
+    }
+
 }
